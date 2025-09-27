9f9fd77c6145107d5cc9f06147ac92a945976b7a
Jason Lim Chin Seong <jason.lim8873@gmail.com>
2025-09-27 15:49:53 +1000

    fixed naming typo

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,4 @@
-var vg_1 = "aus_rentention_rates.vg.json";
+var vg_1 = "aus_retention_rates.vl.json";
 vegaEmbed("#map", vg_1).then(function(result) {
-// Access the Vega view instance
-(https://vega.github.io/vega/docs/api/view/) as result.view
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

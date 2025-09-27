42a18cd191f54024549fdf5a158daefebd578158
Jason Lim Chin Seong <118088123+jasonlimcs@users.noreply.github.com>
2025-09-27 15:33:04 +1000

    Fix filename extension from .vl.json to .vg.json

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "aus_rentention_rates.vl.json";
+var vg_1 = "aus_rentention_rates.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view

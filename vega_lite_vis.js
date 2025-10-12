62b3cc4e8523e53e0ece05548aa62fd71c0f0e84
Jason Lim Chin Seong <jason.lim8873@gmail.com>
2025-10-13 00:30:28 +1100

    fixed layout

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,14 +1,14 @@
 var vg_1 = "aus_retention_rates.vl.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "dumbbell.vl.json";
-vegaEmbed("#equity", vg_2).then(function(result) {
+vegaEmbed("#equity", vg_2, {"actions": false}).then(function(result) {
 	// Embedded dumbbell chart
 }).catch(console.error);
 
 var vg_3 = "dotplot.vl.json";
-vegaEmbed("#dotplot", vg_3).then(function(result) {
+vegaEmbed("#dotplot", vg_3, {"actions": false}).then(function(result) {
     // Embedded dot plot
 }).catch(console.error);

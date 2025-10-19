ea4d1d262dbc884baaa090bb4ae6c0734c8b9968
Jason Lim Chin Seong <jason.lim8873@gmail.com>
2025-10-19 11:16:22 +1100

    added charts

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -12,3 +12,13 @@ var vg_3 = "dotplot.vl.json";
 vegaEmbed("#dotplot", vg_3, {"actions": false}).then(function(result) {
     // Embedded dot plot
 }).catch(console.error);
+
+var vg_4 = "stackedbarchart.vl.json";
+vegaEmbed("#stacked-bar-chart", vg_4, {"actions": false}).then(function(result) {
+    // Embedded stacked bar chart
+}).catch(console.error);
+
+var vg_5 = "remoteness.vl.json";
+vegaEmbed("#remoteness", vg_5, {"actions": false}).then(function(result) {
+    // Embedded remoteness chart
+}).catch(console.error);
